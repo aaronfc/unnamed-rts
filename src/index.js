26b4906c8f4057ce9df96696af3610bae052816d
aaron <yo@aaron.com.es>
2020-04-21 19:44:13 +0200

    Making villagers generic to support multiple villagers. Now the main building creates villagers upon click

--- src/index.js
+++ src/index.js
@@ -20,37 +20,48 @@ function preload() {}
 
 function create() {
 
+  this.villagers = [];
+
   // Create main building
   var mainBuilding = this.add.rectangle(0, 0, 100, 50, "0x0000FF");
   mainBuilding.setOrigin(0,0);
+  mainBuilding.setInteractive();
+  mainBuilding.on('pointerdown', (pointer, localX, localY, event) => {
+    var newPosition = new Phaser.Math.Vector2(
+      mainBuilding.x + mainBuilding.width + 10,
+      mainBuilding.y + mainBuilding.height + 10
+    );
+    this.villagers.push(new Villager(this, newPosition.x, newPosition.y));
+    event.stopPropagation();
+  });
   
   // Create Adan
-  var villagerPosition = new Phaser.Math.Vector2(
+  var newPosition = new Phaser.Math.Vector2(
     mainBuilding.x + mainBuilding.width + 10,
     mainBuilding.y + mainBuilding.height + 10
   );
-  this.villager = new Villager(this, villagerPosition.x, villagerPosition.y);
+  this.villagers.push(new Villager(this, newPosition.x, newPosition.y));
   // Input
   this.input.mouse.disableContextMenu();
   this.input.on('pointerdown', (pointer) => {
-        if (pointer.rightButtonDown())
-        {
-            if (this.villager.selected)
-            {
-                this.villager.destination = new Phaser.Math.Vector2(pointer.x, pointer.y);
-                this.villager.selected = false;
-            }
+
+    // For any selected villager
+    // TODO: Improve performance on this. Instead of iterating over all villagers, have a separate variable
+    // with the selected villagers. They can be appended themselves upon click.
+    this.villagers.filter(v => v.selected)
+      .forEach( v => {
+        if (pointer.rightButtonDown()) {
+          v.destination = new Phaser.Math.Vector2(pointer.x, pointer.y);
+          v.selected = false;
         }
-        if (pointer.leftButtonDown())
-        {
-            if (this.villager.selected)
-            {
-                this.villager.selected = false;
-            }
+        if (pointer.leftButtonDown()) {
+          v.selected = false;
         }
+      });
     });
 }
 
 function update() {
-  this.villager.update();
+  this.villagers.forEach( v => v.update());
 }
+
