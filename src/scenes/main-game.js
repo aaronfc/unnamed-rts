b606e0e120874b280d90e06d3774937316de128c
aaron <yo@aaron.com.es>
2020-05-23 18:45:55 +0200

    Removing initial enemies

--- src/scenes/main-game.js
+++ src/scenes/main-game.js
@@ -59,8 +59,8 @@ export default class MainGameScene extends Phaser.Scene {
 
     // Create bad guy
     let newBadGuyPosition = {x: 500, y: 500};
-    this.enemies.push(new Enemy(this, newBadGuyPosition.x, newBadGuyPosition.y));
-    this.enemies.push(new Enemy(this, newBadGuyPosition.x, newBadGuyPosition.y));
+    //this.enemies.push(new Enemy(this, newBadGuyPosition.x, newBadGuyPosition.y));
+    //this.enemies.push(new Enemy(this, newBadGuyPosition.x, newBadGuyPosition.y));
 
     // Resource
     var resource = new Resource(this, 200, 200, 1000);
