0db98514a1982b5ee610b48072cc1f2bdc0e8f32
aaron <yo@aaron.com.es>
2020-05-23 20:17:41 +0200

    Setting everything up for github.io

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cd dist/
+git add --all
+git commit -m "Deploy"
+git push origin gh-pages
--- webpack/prod.js
+++ webpack/prod.js
@@ -2,34 +2,38 @@ const merge = require("webpack-merge");
 const path = require("path");
 const base = require("./base");
 const TerserPlugin = require("terser-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CreateFileWebpack = require("create-file-webpack");
 
 module.exports = merge(base, {
   mode: "production",
   output: {
-    filename: "bundle.min.js"
+    filename: "bundle.min.js",
   },
   devtool: false,
   performance: {
     maxEntrypointSize: 900000,
-    maxAssetSize: 900000
+    maxAssetSize: 900000,
   },
   optimization: {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
-    ]
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'assets', to: 'assets' }
-      ]
-    })
-  ]
+      patterns: [{ from: "assets", to: "assets" }],
+    }),
+    new CreateFileWebpack({
+      path: "./dist",
+      fileName: ".git",
+      content: "gitdir: ../.git/worktrees/dist",
+    }),
+  ],
 });
